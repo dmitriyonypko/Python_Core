894be300d480fc1df5e99448274541d4a0db2c7a
Dmitriy Onypko <thetwins7777@gmail.com>
2020-02-05 23:21:07 +0200

    add project

--- Les_12_final_project/class_contacts.py
+++ Les_12_final_project/class_contacts.py
@@ -3,33 +3,31 @@
 Добавление контакта, удаление контакта, редактирование контакта.
 Поиск по имени-фамилии, поиск по номеру, поиск по частично набранному номеру
 """
-# Доделать поиск по компании
 
 
 class Contact:
     
     def __init__(self, file_name):
         self.file_name = file_name
-        # Нам это нужно ??????
         self.header = []
         self.body = []
-        # Нужен для поиска
         self.searched_list = []
+        self.number_for_print = 0
     
     def __str__(self):
-        if len(self.searched_list) != 0:
-            print('__str__ для поиска')
+        if self.number_for_print:
             return f'{self.__table_for_print(self.header, self.searched_list)}'
         else:
-            print('__str__ для всей таблицы')
-            self.__open_file_read()
+            self.__read_file()
             return f'{self.__table_for_print(self.header, self.body)}'
     
     def __table_for_print(self, header, body):
-        """Делаем из файла таблицу для печати"""
+        """Делаем из двух списков таблицу для печати."""
+        
         list_lines = []
         list_lines.append(header)
         list_lines.extend(body)
+        
         # Создаем словарь {индекс: max длина слова}
         len_words = {}
         for line in list_lines:
@@ -39,6 +37,7 @@ class Contact:
                     len_words[index] = len_word
                 if len_words[index] < len_word:
                     len_words[index] = len_word
+        
         # Записываем таблицу в строку
         count_char = '\n' + '-' * (sum(len_words.values()) + len(list_lines[0]) * 3 - 1) + '\n'
         result_table = ''
@@ -47,32 +46,34 @@ class Contact:
             for index, word in enumerate(line):
                 result_table += str(word).ljust(len_words[index]) + ' | '
         result_table += count_char
+        
         return result_table
     
-    def __open_file_read(self):
-        """Открываем файл."""
+    def __read_file(self):
+        """Считывает данные из файла."""
         with open(self.file_name, 'r', encoding='utf-8') as file:
             self.header = file.readline()
             self.header = self.header.strip().split(',')
             self.body = [line.strip().split(',') for line in file]
     
     def __write_data(self):
-        """Записываем данные в файл."""
+        """Записывает данные в файл."""
         self.body = list(map(','.join ,self.body))
         with open(self.file_name, 'w', encoding='utf-8') as file:
             file.write(f"{','.join(self.header)}\n")
             file.write('\n'.join(self.body))
-
+    
     def __edit_id(self, id_contact):
         """Пересчитываем id. """
         for item in self.body[id_contact:]:
             item[0] = str(id_contact + 1)
             id_contact += 1
     
-    def search_contact(self, data_for_search, index_for_search=2):
-        """Ретурн найденный или пустой список"""
+    def search_contact(self, data_for_search, index_for_search=5):
+        """Возвращает список с найденным контактом или возвращает пустой список."""
+        self.number_for_print = 1
         self.searched_list = []
-        self.__open_file_read()
+        self.__read_file
         
         if index_for_search == 4:
             index = 3
@@ -80,7 +81,7 @@ class Contact:
             index = 0
         elif data_for_search.isdigit():
             index = 2
-        elif '@' in data_for_search:
+        elif index_for_search == 5:
             index = 4
         else:
             index = 1
@@ -91,13 +92,15 @@ class Contact:
                     self.searched_list.append(line)
             elif data_for_search.lower() in line[index].lower():
                 self.searched_list.append(line)
+        
         return self.searched_list
     
     def append_contact(self, name='-', phone_number='0', company='-', email='-'):
         """Добавляем контакт если в справочнике нет имени, телефона, email"""
+        # Не работает проверка!!!!
         search_name = len(self.search_contact(name))
         search_number = len(self.search_contact(phone_number))
-        search_email = len(self.search_contact(email))
+        search_email = len(self.search_contact(email, 5))
         self.searched_list = []
         self.new_id = len(self.body) + 1
         
@@ -109,21 +112,34 @@ class Contact:
             return True
     
     def del_contact(self, id_contact):
-        """Удаляем контакт по заданому id."""
-        self.__open_file_read()
+        """Удаляет контакт по заданому id."""
         id_contact = int(id_contact)
         if id_contact not in range(1, len(self.body) + 1):
             return False
+        
+        self.__read_file()
+        
         del self.body[id_contact - 1]
         self.__edit_id(id_contact - 1)
         self.__write_data()
+        
         return True
-
+    
     def edit_contact(self, id_contact, index_for_edit, data):
-        """Рудактируем контакт."""
-        self.__open_file_read()
+        """Рудактирует контакт."""
         if id_contact not in range(1, len(self.body) + 1):
             return False
+        
+        # Проверяем не будет ли дублирующих строк в файле. Если будут, вернуть False
+        search_data = self.search_contact(data)
+        if search_data:
+            new_line = []
+            new_line.extend(self.body[id_contact - 1])
+            new_line[index_for_edit] = data
+            for line in search_data:
+                if line[1:] == new_line[1:]:
+                    return False
+        
         self.body[id_contact - 1][index_for_edit] = data
         self.__write_data()
         return True
--- Les_12_final_project/interface.py
+++ Les_12_final_project/interface.py
@@ -3,63 +3,67 @@
 Добавление контакта, удаление контакта, редактирование контакта.
 Поиск по имени-фамилии, поиск по номеру, поиск по частично набранному номеру
 """
-# TODO: После поиска не выводится вся таблица
-# Из за метода __str__, там стоит фильтр. Если поиск проводился, то выводим результаты поиска.
+
 
 from class_contacts import *
 
 
 cont = Contact('contacts.txt')
-print(cont)
 
 
 while True:
     option = int(input('\nВыберите действие:\n1 - Поиск\n2 - Добавить контакт\n3 - Удалить контакт\n4 - Редактировать контакт\n5 - Показать справочник\n6 - Выход\n----> '))
     if option not in range(1, 7):
         print('\nНекорректный ввод')
-    
     if option == 1:
-        index_for_search = int(input('Что ищем ?\n1 - id\n2 - Имя/Фамилию\n3 - Номер телефона\n4 - Компанию\n5 - E-mail\n----> '))
+        index_for_search = int(input('\nЧто ищем ?\n1 - id\n2 - Имя/Фамилию\n3 - Номер телефона\n4 - Компанию\n5 - E-mail\n----> '))
         if index_for_search not in range(1, 6):
-            print('Некорректный ввод\n')
+            print('\nНекорректный ввод')
         else:
-            data_for_search = input('Введите данные для поиска: ')
+            data_for_search = input('\nВведите данные для поиска: ')
             if index_for_search == 1:
                 data_for_search = int(data_for_search)
             if len(cont.search_contact(data_for_search, index_for_search)) != 0:
                 print(cont)
             else:
-                print('По Вашему запросу ничего не найдено.')
+                print('\nПо Вашему запросу ничего не найдено.')
     elif option == 2:
         name = input('Введите имя и фамилию: ')
         number = input('Введите номер телефона: ')
         company = input('Введите название компании: ')
         email = input('Введите E-mail: ')
         if not number.isdigit():
-            print('Некорректный ввод. Номер должен состоять только из цифр!\n')
+            print('\nНекорректный ввод. Номер должен состоять только из цифр!\n')
+        elif '@' not in email:
+            print("\nНекорректный ввод. E-mail должен содержать символ '@'!\n")
         else:
             if cont.append_contact(name, number, company, email):
-                print('Данные успешно добавлены')
+                print('\nДанные успешно добавлены')
             else:
-                print('Контакт уже есть в справочнике')
+                print('\nКонтакт уже есть в справочнике')
     elif option == 3:
         id_contact = int(input('Введите id контакта: '))
         if cont.del_contact(id_contact):
-            print('Контакт успешно удален')
+            print('\nКонтакт успешно удален')
         else:
-            print('Некорректно введен id')
+            print('\nНекорректно введен id')
     elif option == 4:
         index_for_edit = int(input('Что редактируем ?\n1 - Имя/Фамилию\n2 - Номер телефона\n3 - Компанию\n4 - E-mail\n----> '))
         if index_for_edit not in range(1, 5):
-            print('Некорректный ввод\n')
+            print('\nНекорректный ввод\n')
         else:
+            # Не работает!!!!!
             id_contact = int(input('Введите id контакта: '))
-            data = input('Введите данные: ')
-            if cont.edit_contact(id_contact, index_for_edit, data):
-                print('Данные успешно обновлены')
+            if index_for_search not in range(1, 6):
+                print('\nНекорректный ввод')
             else:
-                print('Некорректно введен id')
+                data = input('Введите данные: ')
+                if cont.edit_contact(id_contact, index_for_edit, data):
+                    print('\nДанные успешно обновлены')
+                else:
+                    print('\nКонтакт уже есть в справочнике')
     elif option == 5:
+        cont.number_for_print = 0
         print(cont)
     elif option == 6:
         break
